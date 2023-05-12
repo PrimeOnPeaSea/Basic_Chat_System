65b2d2829ae35b2d3cf8c1e27d60f45767dbd990
ParthSingh <parth.singh1023@gmail.com>
2023-05-13 01:08:36 +0530

    New Server

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://localhost:5000',
+      'https://basic-server-2gwl.onrender.com:443',
       { query: { id } }
     )
     setSocket(newSocket)
